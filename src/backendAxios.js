6a6c9b648fbf79c3170a59eff35e90435a642f58
visshal14 <vishalpal2912@gmail.com>
2023-07-22 14:13:18 +0530

    Update backendAxios.js

--- src/backendAxios.js
+++ src/backendAxios.js
@@ -4,7 +4,23 @@ const instance = axios.create({
 
     baseURL: "http://localhost:3001/api/v1",
     // baseURL: "https://watcher-backend.onrender.com/api/v1",
-    headers: { 'authorization': `Bearer ${localStorage.getItem("accessToken")}` }
+    headers: {
+        'authorization': `Bearer ${localStorage.getItem("accessToken")}`,
+
+    }
 
 })
+
+
+instance.interceptors.request.use(
+    (config) => {
+
+        config.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+        return config;
+    },
+    (error) => {
+        // console.log("request error", error);
+        return Promise.reject(error);
+    }
+);
 export default instance;
